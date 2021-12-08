491b6db243dde25fdfb8120a7d9b27e40485df85
Koban Davis <kobandavis@gmail.com>
2021-12-08 09:58:17 +0000

    solve day 8

---
+++ src/8_1.ts
@@ -0,0 +1,11 @@
+import { useFile } from './help'
+
+const inputs = useFile('8.txt')
+	.split('\r\n')
+	.flatMap((row) => row.split(' | ')[1].split(' '))
+
+// [1,4,7,8]
+const lengths = new Set([2, 4, 3, 7])
+const occurances = inputs.reduce((total, input) => (lengths.has(input.length) ? total + 1 : total), 0)
+
+console.log(inputs, occurances)
---
+++ src/8_2.ts
@@ -0,0 +1,100 @@
+import { useFile } from './help'
+
+let inputRows = useFile('8.txt').split('\r\n')
+
+let inputs = inputRows.map((row) =>
+	row
+		.split(' | ')[0]
+		.split(' ')
+		.map((number) => number.split(''))
+)
+let outputs = inputRows.map((row) =>
+	row
+		.split(' | ')[1]
+		.split(' ')
+		.map((number) => number.split(''))
+)
+
+// 2 -> 1
+// 3 -> 7
+// 4 -> 4
+// 5 -> 2, 3, 5
+// 6 -> 6, 9, 0
+// 7 -> 8
+
+const solveSignals = (input: string[][]) => {
+	const numberMap = new Map<number, string[]>()
+	const sorted = input.sort((a, b) => (a.length > b.length ? 1 : -1))
+
+	const len5 = sorted.slice(3, 6)
+	const len6 = sorted.slice(6, 9)
+
+	numberMap.set(1, sorted[0])
+	numberMap.set(7, sorted[1])
+	numberMap.set(4, sorted[2])
+	numberMap.set(8, sorted[9])
+	numberMap.set(
+		3,
+		len5.splice(
+			len5.findIndex((input) => numberMap.get(1).every((v) => input.includes(v))),
+			1
+		)[0]
+	)
+	numberMap.set(
+		5,
+		len5.splice(
+			len5.findIndex((input) => {
+				const one = numberMap.get(1)
+				const four = numberMap.get(4).slice()
+				one.forEach((segment) =>
+					four.splice(
+						four.findIndex((v) => segment === v),
+						1
+					)
+				)
+				return four.every((v) => input.includes(v))
+			}),
+			1
+		)[0]
+	)
+	numberMap.set(2, len5[0])
+	numberMap.set(
+		9,
+		len6.splice(
+			len6.findIndex((input) => [...numberMap.get(5), ...numberMap.get(1)].every((v) => input.includes(v))),
+			1
+		)[0]
+	)
+	numberMap.set(
+		6,
+		len6.splice(
+			len6.findIndex((input) => numberMap.get(5).every((v) => input.includes(v))),
+			1
+		)[0]
+	)
+	numberMap.set(0, len6[0])
+	return numberMap
+}
+
+let total = 0
+for (let i = 0; i < inputs.length; i++) {
+	const input = inputs[i]
+	const output = outputs[i]
+
+	const numberMap = solveSignals(input)
+	const stringMap: Record<string, number> = {}
+	for (const [number, segmentArr] of numberMap.entries()) {
+		stringMap[segmentArr.sort().join('')] = number
+	}
+
+	const value = output
+		.map((o) => o.sort().join(''))
+		.reduce((total, string) => {
+			// console.log(string, stringMap)
+			return total + stringMap[string].toString()
+		}, '')
+	console.log(value)
+	total += Number(value)
+}
+
+console.log(total)
